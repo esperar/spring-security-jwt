ef351a36c3e6910ad0386492607da0073cd89667
KimHuiMang <piyrw9754@gmail.com>
2022-11-30 23:28:37 +0900

    UserService 비즈니스 로직 작성

---
+++ src/main/java/com/securityhope/hope/service/impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.securityhope.hope.service.impl;
+
+import com.securityhope.hope.domain.Role;
+import com.securityhope.hope.domain.User;
+import com.securityhope.hope.repository.RoleRepository;
+import com.securityhope.hope.repository.UserRepository;
+import com.securityhope.hope.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Override
+    public User saveUser(User user) {
+         return userRepository.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
