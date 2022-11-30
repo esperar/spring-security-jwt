3239c43700723d93889af181271b4cf4beadafa4
KimHuiMang <piyrw9754@gmail.com>
2022-11-30 23:06:34 +0900

    project setting

---
+++ src/test/java/com/securityhope/hope/HopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityhope.hope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
