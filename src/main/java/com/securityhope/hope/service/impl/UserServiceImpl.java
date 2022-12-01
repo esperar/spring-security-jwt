bff26a76bf5982d7dac75155852562d30411ecac
KimHuiMang <piyrw9754@gmail.com>
2022-12-01 23:32:33 +0900

    userService password encoding 적용

--- src/main/java/com/securityhope/hope/service/impl/UserServiceImpl.java
+++ src/main/java/com/securityhope/hope/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +27,7 @@ public class UserServiceImpl implements UserService , UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -46,6 +48,7 @@ public class UserServiceImpl implements UserService , UserDetailsService {
     @Override
     public User saveUser(User user) {
         log.info("save user {}", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()) );
         return userRepository.save(user);
     }
 
