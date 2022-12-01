2042bbddaaac1ae0a301b20de4fb3ad4dfc3cb2c
KimHuiMang <piyrw9754@gmail.com>
2022-12-01 14:01:17 +0900

    ResponseEntity를 통한 api 응답

---
+++ src/main/java/com/securityhope/hope/api/UserResource.java
@@ -0,0 +1,24 @@
+package com.securityhope.hope.api;
+
+import com.securityhope.hope.domain.User;
+import com.securityhope.hope.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserResource {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
