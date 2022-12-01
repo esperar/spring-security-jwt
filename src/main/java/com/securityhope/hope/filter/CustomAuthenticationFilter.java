f86570d362237463fcb769f588b9e93a80cab674
KimHuiMang <piyrw9754@gmail.com>
2022-12-01 23:28:30 +0900

    JSON 형으로 token 발급

--- src/main/java/com/securityhope/hope/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/securityhope/hope/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.securityhope.hope.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,9 +16,14 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @Slf4j
@@ -55,7 +61,13 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(request.getRequestURI().toString())
                 .sign(algorithm);
 
-        response.setHeader("access_token", accessToken);
-        response.setHeader("refresh_token" , refreshToken);
+        /*response.setHeader("access_token", accessToken);
+        response.setHeader("refresh_token" , refreshToken);*/
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", accessToken);
+        tokens.put("refresh_token" , refreshToken);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream() , tokens);
     }
 }
