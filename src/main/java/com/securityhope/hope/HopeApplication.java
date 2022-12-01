a7becf20225689e38970ef573a33157d86d11e17
KimHuiMang <piyrw9754@gmail.com>
2022-12-01 17:59:32 +0900

    Default data 주입

--- src/main/java/com/securityhope/hope/HopeApplication.java
+++ src/main/java/com/securityhope/hope/HopeApplication.java
@@ -1,13 +1,43 @@
 package com.securityhope.hope;
 
+import com.securityhope.hope.domain.Role;
+import com.securityhope.hope.domain.User;
+import com.securityhope.hope.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class HopeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HopeApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "Kim hope", "kim", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Park juhong", "park", "2345", new ArrayList<>()));
+			userService.saveUser(new User(null, "Yun taebin", "yun", "3456", new ArrayList<>()));
+			userService.saveUser(new User(null, "chae jongin", "chae", "4567", new ArrayList<>()));
+
+			userService.addRoleToUser("kim", "ROLE_USER");
+			userService.addRoleToUser("park", "ROLE_MANAGER");
+			userService.addRoleToUser("yun", "ROLE_ADMIN");
+
+			userService.addRoleToUser("chae", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("chae", "ROLE_MANAGER");
+			userService.addRoleToUser("chae", "ROLE_ADMIN");
+		};
 	}
 
 }
